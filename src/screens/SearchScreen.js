210bddbb83bb1a826640a87b16775dc11c1d03a8
manii_sd <manii.shahdoust@gmail.com>
2021-08-27 16:30:08 +0430

    add: default value 'pasta'

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../Components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,22 +6,36 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMassage, serErrorMassage] = useState('');
 
-  const serachApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      },
-    });
-    setResults(response.data.businesses);
+  const serachApi = async searchTerm => {
+    console.log('hi there!');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      serErrorMassage('something went wrong');
+    }
   };
+
+  useEffect(() => {
+    serachApi('pasta');
+  }, []);
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSumit={serachApi} />
-      <Text> serach bar </Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSumit={() => serachApi(term)}
+      />
+      {errorMassage ? <Text>{errorMassage}</Text> : null}
       <Text>we have found {results.length} results</Text>
     </View>
   );
