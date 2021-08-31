d4640f67ab3a46a6fe77e6f8e8e6415c1b436284
manii_sd <manii.shahdoust@gmail.com>
2021-08-31 21:05:22 +0430

    create app

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-import SearchScreen from './src/screens/SearchScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import ResultShowScreen from './src/screens/ResultShoeScreen';
+import IndexScreen from './src/screens/indexScreen';
 
 const navigator = createStackNavigator(
   {
-    Search: SearchScreen,
-    Home: HomeScreen,
-    ResultsShow: ResultShowScreen,
+    Index: IndexScreen,
   },
   {
-    initialRouteName: 'Search',
+    initialRouteName: 'Index',
     defaultNavigationOptions: {
-      title: 'Business Search',
+      title: 'Blogs',
     },
   },
 );
--- src/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization:
-      'Bearer onqvgMMI9jBMGAwL3x_esUrFtLsg_FYDI-Kyk_Mh7Jf0aeEYXHlM4NIpZSQc192ICSI1w6Qj2V8WSAlpcB65w999BAc7IVAG7ihCuoDvqq6T3RE-bkPuX2x1OIwfYXYx',
-  },
-});
